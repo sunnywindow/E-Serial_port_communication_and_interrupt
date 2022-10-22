ed22b40914f6b0c8ad437212c2e8cfd4e2b9b730
sunnywindows <hye4vq4zz@163.com>
2022-10-22 11:22:02 +0800

    2022/10/22

---
+++ Serial_port_communication/MDK-ARM/RTE/_Serial_port_communication/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Serial_port_communication' 
+ * Target:  'Serial_port_communication' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
